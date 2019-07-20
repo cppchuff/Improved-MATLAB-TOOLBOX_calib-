4e7a2b1d62c5bf66a653c7be58997d874a48b6ec
littlepiper <34118378+littlepiper@users.noreply.github.com>
2019-07-20 17:17:29 +0800

    Update click_ima_calib_fisheye_no_read.m

--- click_ima_calib_fisheye_no_read.m
+++ click_ima_calib_fisheye_no_read.m
@@ -1,23 +1,4 @@
-% Rough estimates for principal point and focal length:
-% fg = [1009.661057548567   1009.706970843858 ]';
-% cg = [ 1031.688786545889   1288.395050759215]';
-% kg = [-0.054285891395760  -0.026812583950935 0 0]';
-
-% % wuyuan fisheye test
-% fg = [320   280 ]';
-% cg = [ 640   360]';
-% kg = [0.2  -0.1 0.04 -0.008]';
-
-%opencv init
-% fg = [350.438926029233   350.2174211053091 ]';
-% cg = [643.9272197363401   359.684099539598]';
-% kg = [0.121311, -0.0902425, 0.10761, -0.0461907]';
-
-% fg = [320.88785   320.48022 ]';
-% cg = [656.05855   379.36670]';
-% kg = [0.06336   0.06080   -0.03398   0.00450]';
-
-%%%最近使用的参数
+%楸肩溂鐩告満鍙傛暟璁剧疆
 fg = [377.52445   363.102738465345680  ]';
 cg = [637.656119618039840  367.716794364937930 ]';
 kg = [-0.052199196426168  0.020099287047905  -0.018613380783400  0.004923019763423]';
@@ -26,106 +7,104 @@ grid_success = 0;
 
 
 while (~grid_success)
+
+    fprintf(1,['Number of squares along the X direction ([]=' num2str(n_sq_x_default) ') = ']);
+    n_sq_x = input(''); %6
+
+    if isempty(n_sq_x), n_sq_x = n_sq_x_default; end;
+    fprintf(1,['Number of squares along the Y direction ([]=' num2str(n_sq_y_default) ') = ']);
+    n_sq_y = input(''); %6
+
+    if isempty(n_sq_y), n_sq_y = n_sq_y_default; end;
+    Np = (n_sq_x+1)*(n_sq_y+1);
+    eval(['x_' num2str(kk) ' =  zeros(2,' num2str(Np) ') ;']);
+        
+    grid_success = 1;
+
+    figure(2); clf;
+    image(uint8(I));
+    axis image;
+    colormap(map);
+    set(2,'color',[1 1 1]);
+    x=zeros(2,Np);y = zeros(2,Np);
+    figure(2); hold on;
+    v0=0;
+    [X,Y,cropimg,RECT] = imcrop(uint8(I));
+    corners = findCorners(cropimg,0.05,1);
+    chessboards = chessboardsFromCorners(corners);
+        
+    x_fc = zeros(4,1);
+    y_fc = zeros(4,1);
     
-    if  1%manual_squares,
-        fprintf(1,['Number of squares along the X direction ([]=' num2str(n_sq_x_default) ') = ']);
-        n_sq_x = input(''); %6
-
-        if isempty(n_sq_x), n_sq_x = n_sq_x_default; end;
-        fprintf(1,['Number of squares along the Y direction ([]=' num2str(n_sq_y_default) ') = ']);
-        n_sq_y = input(''); %6
-
-        if isempty(n_sq_y), n_sq_y = n_sq_y_default; end;
-        Np = (n_sq_x+1)*(n_sq_y+1);
-        eval(['x_' num2str(kk) ' =  zeros(2,' num2str(Np) ') ;']);
-        
-        grid_success = 1;
-
-        figure(2); clf;
-        image(uint8(I));
-        axis image;
-        colormap(map);
-        set(2,'color',[1 1 1]);
-        x=zeros(2,Np);y = zeros(2,Np);
-        figure(2); hold on;
-        v0=0;
-        [X,Y,cropimg,RECT] = imcrop(uint8(I));
-        corners = findCorners(cropimg,0.05,1);
-        chessboards = chessboardsFromCorners(corners);
-        
-        x_fc = zeros(4,1);
-        y_fc = zeros(4,1);
-    
-        for i=1:length(chessboards)
-            % extract chessboard
-            cb = chessboards{i};
-            A=zeros(size(cb,1),size(cb,2),2);
-            for j=1:size(cb,1)
-                p = corners.p(cb(j,:),:)+1;
-                %plot(RECT(1)+p(:,1),RECT(2)+v0+p(:,2),'o','Color','r','LineWidth',2);
-                A(j,:,:)=[RECT(1)+p(:,1),RECT(2)+v0+p(:,2)];
-            end
+    for i=1:length(chessboards)
+        % extract chessboard
+        cb = chessboards{i};
+        A=zeros(size(cb,1),size(cb,2),2);
+        for j=1:size(cb,1)
+            p = corners.p(cb(j,:),:)+1;
+            %plot(RECT(1)+p(:,1),RECT(2)+v0+p(:,2),'o','Color','r','LineWidth',2);
+            A(j,:,:)=[RECT(1)+p(:,1),RECT(2)+v0+p(:,2)];
         end
-        ns=size(A);
-        if(ns(1)~=n_sq_y_default+1)
-            A=permute(A,[2 1 3]);
-            A=flipud(A);
-        end
-        for j=1:size(A,1)
-            plot(A(j,:,1),A(j,:,2),'o','Color','r','LineWidth',2);
-        end
-        [hang,lie,ch]=size(A);
-        if(hang*lie~=Np)
-            error('detect chessboard size error!');
-        end
-        for j=1:Np
-            if(mod(j,lie)==0)
-                hi=floor(j/lie);
-                hj=lie;
-            else
-                hi=floor(j/lie)+1;
-                hj=mod(j,lie);
-            end
-            text(A(hi,hj,1),A(hi,hj,2)+20,num2str(j),'horiz','center','color','r');
-        end
-        hold off;
-        figure(2); 
-        image(I);
-        colormap(map);
-        hold on;
-
-        hx = plot(zeros(1,Np),zeros(1,Np),'r+');
-        hcp = plot(1,1,'co');
-
-        hold off;
-        
-        for np = 1:Np
-            set(hcp,'Xdata',x(1,np)+1,'Ydata',x(2,np)+1);
-
-            % [xi,yi,b] = ginput4(1);
-            if(mod(np,lie)==0)
-                hi=floor(np/lie);
-                hj=lie;
-            else
-                hi=floor(np/lie)+1;
-                hj=mod(np,lie);
-            end
-            b=1;
-            xi=A(hi,hj,1);
-            yi=A(hi,hj,2);
-            if b==1,
-                xxi = [xi;yi];
-                if q_converge,
-                    [xxi] = cornerfinder(xxi,I,winty,wintx);
-                end;
-                x(1,np) = xxi(1) - 1;
-                x(2,np) = xxi(2) - 1;
-                set(hx,'Xdata',x(1,:)+1,'Ydata',x(2,:)+1);
-                x_fc =[x(1,end-n_sq_x),x(1,1) ,x(1,1+n_sq_x),x(1,end)]';
-                y_fc =[x(2,end-n_sq_x),x(2,1) ,x(2,1+n_sq_x),x(2,end)]';    
-            end;
-        end;
     end
+    ns=size(A);
+    if(ns(1)~=n_sq_y_default+1)
+        A=permute(A,[2 1 3]);
+        A=flipud(A);
+    end
+    for j=1:size(A,1)
+        plot(A(j,:,1),A(j,:,2),'o','Color','r','LineWidth',2);
+    end
+    [hang,lie,ch]=size(A);
+    if(hang*lie~=Np)
+        error('detect chessboard size error!');
+    end
+    for j=1:Np
+        if(mod(j,lie)==0)
+            hi=floor(j/lie);
+            hj=lie;
+        else
+            hi=floor(j/lie)+1;
+            hj=mod(j,lie);
+        end
+        text(A(hi,hj,1),A(hi,hj,2)+20,num2str(j),'horiz','center','color','r');
+    end
+    hold off;
+    figure(2); 
+    image(I);
+    colormap(map);
+    hold on;
+
+    hx = plot(zeros(1,Np),zeros(1,Np),'r+');
+    hcp = plot(1,1,'co');
+
+    hold off;
+       
+    for np = 1:Np
+        set(hcp,'Xdata',x(1,np)+1,'Ydata',x(2,np)+1);
+
+        % [xi,yi,b] = ginput4(1);
+        if(mod(np,lie)==0)
+            hi=floor(np/lie);
+            hj=lie;
+        else
+            hi=floor(np/lie)+1;
+            hj=mod(np,lie);
+        end
+        b=1;
+        xi=A(hi,hj,1);
+        yi=A(hi,hj,2);
+        if b==1,
+            xxi = [xi;yi];
+            if q_converge,
+                [xxi] = cornerfinder(xxi,I,winty,wintx);
+            end;
+            x(1,np) = xxi(1) - 1;
+            x(2,np) = xxi(2) - 1;
+            set(hx,'Xdata',x(1,:)+1,'Ydata',x(2,:)+1);
+            x_fc =[x(1,end-n_sq_x),x(1,1) ,x(1,1+n_sq_x),x(1,end)]';
+            y_fc =[x(2,end-n_sq_x),x(2,1) ,x(2,1+n_sq_x),x(2,end)]';
+        end;
+    end;
     
     
     
